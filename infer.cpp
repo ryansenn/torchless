16b5cbf1fa65e89d7ed03d717277ed3440452a32
ryan <ryansen87@gmail.com>
2025-05-25 23:02:17 -0700

    rmsnorm

--- infer.cpp
+++ infer.cpp
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <iostream>
 
 // W (d,n) @ x (n,) = xout (d,)
 void matmul(float* xout, float* x, float* w, int n, int d){
@@ -13,7 +14,22 @@ void matmul(float* xout, float* x, float* w, int n, int d){
 }
 
 
-void rmsnorm(float* o, float* x, float* weight, int size, float eps){
+void rmsnorm(float* o, // output
+             float* x, // input
+             float* g, // scale per element
+             int    n, // size
+             float eps) // epsilon 
+{
+    float mean_squared = 0;
+    for (int i=0;i<n;i++){
+        mean_squared += x[i]*x[i];
+    }
+    mean_squared = mean_squared/n;
+    float m = sqrt(mean_squared + eps);
+
+    for (int i=0; i<n; i++){
+        o[i] = x[i] * g[i] / m;
+    }
 
 }
 
--- test.cpp
+++ test.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 
 void matmul(float* xout, float* x, float* w, int n, int d);
+void rmsnorm(float* o, float* x, float* g, int n, float eps);
 
 int success = 1;
+const float tolerance = 1e-6f;
 
 void test_matmul(){
     // W(d,n) @ x(n,) = xout
@@ -32,15 +34,38 @@ void test_matmul(){
 
     for (int i=0; i<d; i++){
        if (xout[i] != expected[i]){
-            std::cout << "matmul test failed " << std::endl;
+            std::cout << "matmul test failed" << std::endl;
             success = 0;
             return;
        }
+    }
+
+    delete[] w; delete[] x; delete[] xout; delete[] expected;
+}
+
+void test_rmsnorm(){
+    int n = 5;
+    float o[n];
+    float x[5] = {1,2,3,4,5};
+    float g[5] = {2,3,1,3,2};
+    float eps = 0;
+
+    float res[5] = {0.6030227, 1.8090681, 0.9045340, 3.6181361, 3.0151134};
+    
+    rmsnorm(o, x, g, n, eps);
+
+    for (int i=0;i<n;i++){
+        if (std::fabs(res[i] - o[i]) > tolerance){
+            std::cout << "rmsnorm test failed" << std::endl;
+            success = 0;
+            return;
+        }
     }
 }
 
 int main() {
     test_matmul();
+    test_rmsnorm();
 
     if (success) {
         std::cout << "all tests passed" << std::endl;
