472a329c343899164740652f1e3f873ae12ab55a
ryan <ryansen87@gmail.com>
2025-09-11 20:03:43 -0700

    Get K,V for each block and write to cache

--- main.cpp
+++ main.cpp
@@ -2,6 +2,25 @@
 #include "model/model.h"
 #include "math_ops.h"
 
+void block_forward(InferenceState& inferenceState, Model& model, int b){
+    // Get Q for the current token
+    matmul(inferenceState.q, *model.blocks[b].wq, inferenceState.x);
+
+    // Get K,V
+    matmul(inferenceState.k, *model.blocks[b].wk, inferenceState.x);
+    matmul(inferenceState.v, *model.blocks[b].wv, inferenceState.x);
+
+    // Compute attention for each head individually
+    for (int i=0; i<model.config.n_heads; i++){
+
+    }
+
+    // Write current K,V to cache
+    inferenceState.k_cache[b]->at({inferenceState.pos}).copy_from(inferenceState.k);
+    inferenceState.v_cache[b]->at({inferenceState.pos}).copy_from(inferenceState.v);
+    inferenceState.pos++;
+}
+
 // Trying first minimal implementation of the inference flow
 void forward(InferenceState& inferenceState, Model& model, int token){
     float* embedding = &model.token_embedding_table->data[token * model.config.hidden_size];
@@ -9,10 +28,10 @@ void forward(InferenceState& inferenceState, Model& model, int token){
 
     rmsnorm(inferenceState.x, inferenceState.x, *model.blocks[0].lm1, model.config.hidden_size);
 
-    // Now starting multi-head attention
-
-    // Get Q for the current token
-    matmul(inferenceState.q, *model.blocks[0].wq, inferenceState.x);
+    // Forward for each block
+    for (int i=0; i<model.config.n_layers; i++){
+        block_forward(inferenceState, model, i);
+    }
 }
 
 int main(){
